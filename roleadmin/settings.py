2ff2c27212a4e7406bb6248ce0d6aaca709da198
dodiman <dodimantakimpoo@gmail.com>
2021-06-17 12:21:33 +0800

    perbaiki setting

--- roleadmin/settings.py
+++ roleadmin/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-qu+1-wjqnm+%dwh$$s(lh^k5ubpls-pt=0#(!@72-yfv#$or0v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['dodiman.pythonanywhere.com']
 
 
 # Application definition
@@ -132,15 +133,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR / 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR / 'assets')
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
